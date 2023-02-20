0205c4a91418e84a50208937a29475c1fc65e988
AdrianHarris96 <97938048+AdrianHarris96@users.noreply.github.com>
2023-02-20 09:48:43 -0500

    K-Means Clustering v1.02
    Update to accept arguments for number of clusters, minimum clusters and maximum clusters

--- k_means_clustering.py
+++ k_means_clustering.py
@@ -2,7 +2,7 @@
 
 #K means clustering - unsupervised learning - grouping with unlabeled data
 
-#Example input: ./k_means_clustering.py -i /Users/adrianharris/Downloads/BMX_G.csv -c1 bmxleg -c2 bmxwaist
+#Example input: ./k_means_clustering.py -i /Users/adrianharris/Documents/ML_introduction/BMX_G.csv -c1 bmxleg -c2 bmxwaist -n 4
 
 #Kmeans example - Clothing Sizes
 import numpy as np
@@ -14,10 +14,14 @@ parser = ap.ArgumentParser(prog = 'K-means Clustering', description='K-means clu
 parser.add_argument("-i", "--input", help="path to input BMX_G CSV", required=True)
 parser.add_argument("-c1", "--col1", help="1st column to extract from dataframe", required=True)
 parser.add_argument("-c2", "--col2", help="2nd column to extract from dataframe", required=True)
+parser.add_argument("-n", "--num_clusters", help="number of clusters", type=int, default=2, required=False)
+parser.add_argument("-m", "--mink", help="minimum k to test", type=int, default=2, required=False)
+parser.add_argument("-M", "--maxk", help="maximum k to test", type=int, default=5, required=False)
 args = parser.parse_args()
 
 input_file = args.input
 df = pd.read_csv(input_file)
+#print(df.head(20))
 
 #Checking for missing values 
 df.isnull().sum()
@@ -32,23 +36,25 @@ df.dropna(subset=[x, y], inplace=True)
 df.isnull().sum() #Checking those columns to ensure all is good
 
 import matplotlib.pyplot as plt
+
+'''
+#Simply xy scatter plot of points
 plt.scatter(df[x], df[y], c='r', s=2)
 plt.xlabel(x)
 plt.ylabel(y)
 plt.show()
-
-quit()
+'''
 
 #Clustering with k_means
 from sklearn.cluster import KMeans
 
-k=2 #This can be provided as input
-feat_matrix = np.array(list(zip(df[x], df[y])))
+k = args.num_clusters
+feat_matrix = np.array(list(zip(df[x], df[y]))) #Zip into feature matrix of 2 features
 
 kmeans = KMeans(n_clusters=k)
 kmeans = kmeans.fit(feat_matrix)
-labels = kmeans.predict(feat_matrix)
-centroids = kmeans.cluster_centers_
+labels = kmeans.predict(feat_matrix) #For linking to corresponding colors while plotting
+centroids = kmeans.cluster_centers_ #For visualization later
 
 #Map colors 
 c = ['b', 'r', 'y', 'g', 'c', 'm']
@@ -63,10 +69,11 @@ from sklearn import metrics
 
 #Finding the optimal K using the silhoutte mean 
 silhoutte_averages = []
-min_k = 2
+min_k = args.mink
+max_k = args.maxk #Note: maxk cannot exceed the number of rows in the matrix
 
 #Loop from minimum k to maximum k (num of rows)
-for k in range(min_k, 10): #max_k can be taken as input
+for k in range(min_k, max_k):
     kmean = KMeans(n_clusters=k).fit(feat_matrix)
     score = metrics.silhouette_score(feat_matrix, kmean.labels_)
     print('Silhoutte coef. for k = {0} is {1}'.format(k, score))
