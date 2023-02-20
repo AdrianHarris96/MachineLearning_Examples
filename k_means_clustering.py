17ab9f251f0518bf9bc8acbeab38e99020b027df
AdrianHarris96 <97938048+AdrianHarris96@users.noreply.github.com>
2023-02-20 09:33:45 -0500

    K-Means Clustering v1.01
    Use argparse to accept arguments for input file and columns to extract

--- k_means_clustering.py
+++ k_means_clustering.py
@@ -2,33 +2,48 @@
 
 #K means clustering - unsupervised learning - grouping with unlabeled data
 
+#Example input: ./k_means_clustering.py -i /Users/adrianharris/Downloads/BMX_G.csv -c1 bmxleg -c2 bmxwaist
+
 #Kmeans example - Clothing Sizes
 import numpy as np
 import pandas as pd
+import argparse as ap
 
-df = pd.read_csv('/Users/adrianharris/Downloads/BMX_G.csv') #This can be provided as an input
+parser = ap.ArgumentParser()
+parser = ap.ArgumentParser(prog = 'K-means Clustering', description='K-means clustering for clothing sizes')
+parser.add_argument("-i", "--input", help="path to input BMX_G CSV", required=True)
+parser.add_argument("-c1", "--col1", help="1st column to extract from dataframe", required=True)
+parser.add_argument("-c2", "--col2", help="2nd column to extract from dataframe", required=True)
+args = parser.parse_args()
+
+input_file = args.input
+df = pd.read_csv(input_file)
 
 #Checking for missing values 
 df.isnull().sum()
 
 #Two columns to pull based on input 
+x = args.col1
+y = args.col2
 
 #Removing nulls from the two relevant columns 
-df.dropna(subset=['bmxleg', 'bmxwaist'], inplace=True)
+df.dropna(subset=[x, y], inplace=True)
 
 df.isnull().sum() #Checking those columns to ensure all is good
 
 import matplotlib.pyplot as plt
-plt.scatter(df['bmxleg'], df['bmxwaist'], c='r', s=2)
-plt.xlabel("leg length (cm)")
-plt.ylabel("waist circum. (cm)")
+plt.scatter(df[x], df[y], c='r', s=2)
+plt.xlabel(x)
+plt.ylabel(y)
 plt.show()
+
+quit()
 
 #Clustering with k_means
 from sklearn.cluster import KMeans
 
 k=2 #This can be provided as input
-feat_matrix = np.array(list(zip(df['bmxleg'], df['bmxwaist'])))
+feat_matrix = np.array(list(zip(df[x], df[y])))
 
 kmeans = KMeans(n_clusters=k)
 kmeans = kmeans.fit(feat_matrix)
@@ -39,7 +54,7 @@ centroids = kmeans.cluster_centers_
 c = ['b', 'r', 'y', 'g', 'c', 'm']
 colors = [c[i] for i in labels]
 
-plt.scatter(df['bmxleg'], df['bmxwaist'], c=colors, s=2)
+plt.scatter(df[x], df[y], c=colors, s=2)
 plt.scatter(centroids[:,0], centroids[:,1], marker='x', s=100, c='black')
 plt.show()
 
